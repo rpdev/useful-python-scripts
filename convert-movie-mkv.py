1787bc7da2bfca4b844914e4d380db1f51dc1741
rpdev <>
2017-05-16 20:43:25 +0200

    Added more files to ignore, checks will now be done against a list

--- convert-movie-mkv.py
+++ convert-movie-mkv.py
@@ -12,18 +12,20 @@ def main(folder):
         if p.wait() != 0:
             print('Error when executing ' + cmd, file=stderr)
         return p.returncode == 0
-    
+
+    skip_files = ['.jpg', '.mkv', '.jpeg', '.xz']
+
     for root, _, files in os.walk(folder):
         for f in files:
             file_path = os.path.join(root, f)
             if not os.path.isfile(file_path):
                 return 1
 
-            if not (f.lower().endswith('.jpg') or f.lower().endswith('.mkv')):
-                mkv_name = os.path.splitext(f)[0] + '.mkv'
+            name_ext = os.path.splitext(f)
+            if name_ext[1].lower() not in skip_files:
+                mkv_name = name_ext[0] + '.mkv'
                 mkv_file_path = os.path.join(root, mkv_name)
-                
-    
+
                 if not _execute_cmd('ffmpeg -i "%s" "%s"' % (file_path, mkv_file_path)):
                     return 1
                 if not _execute_cmd('touch -r "%s" "%s"' % (file_path, mkv_file_path)):
