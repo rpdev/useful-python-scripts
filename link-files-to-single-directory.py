f0f1dfb0e4b81d13b04c6a44febe525f3572d648
rpdev <>
2017-07-22 18:10:46 +0200

    Created simple script for finding all files in a directory tree and linking all these files into a single folder.
    * Changes name of target file, if name clash
    * ignores case on extension
    * uses arguments for source and target folders

---
+++ link-files-to-single-directory.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os
+
+import sys
+
+
+def main(source, target):
+    i = 0
+    for root, _, files in os.walk(source):
+        for f in files:
+            if f.lower().endswith('.gif'):
+                ff = os.path.join(root, f)
+                os.symlink(ff, os.path.join(target, new_name(f, target)))
+
+def new_name(filename, target):
+    num = 1
+    name = filename
+    ext = os.path.splitext(filename)
+    while os.path.exists(os.path.join(target, name)):
+        name = ext[0] + '_' + str(num) + ext[1]
+        num += 1
+    return name
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
